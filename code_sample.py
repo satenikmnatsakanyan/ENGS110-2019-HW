07c86ee5c4cb994e05db20cabd104693ea69b5a8
satenikmnatsakanyan <satenik.mnatsakanyan@gmail.com>
2019-03-20 11:32:20 +0400

    tutti
    hkjhk

---
+++ code_sample.py
@@ -0,0 +1,22 @@
+def setupInitMoney():
+    money = input("Insert the initial balance above 0: ")
+    #TODO: Add a loop to make sure the data is corrct
+    #TODO: Add user input validation
+    #TODO: convert to float before return
+    #TODO: ...
+
+    return money
+
+
+...
+
+def main():
+    print("Describe your app")
+    initMoney = setupInitMoney()
+    percent = setupPercent()
+    years = setupYears()
+    result = calculateTheResultMoney(initMoney, percent, years)
+    ...
+
+
+main()
